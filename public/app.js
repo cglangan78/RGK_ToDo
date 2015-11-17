e1e8732c4340ffd54f04f1d03d25cfbf3f4d3c8b
Kevin <kevinjustinwilliams@gmail.com>
2015-11-17 15:25:28 -0800

    front end done

---
+++ public/app.js
@@ -0,0 +1,52 @@
+// Add functionality to the to-do list application
+var list = $('.list');
+// Remove item
+
+$(list).on('click', '.glyphicon-remove', function() {
+  $(this).parent().remove();
+});
+
+// Finish list item; task will receive strikethrough, move task to the end and 'x' will be replaced with green check mark; unchecking will remove strikethrough.
+
+$(list).on('click', 'input[type="checkbox"]', function() {
+  var $span = $(this).parent().find('span'),
+      $remove = $(this).parent().find('i.glyphicon-remove');
+      $ok = $(this).parent().find('i.glyphicon-ok');
+
+  if ($(this).is(":checked")) {
+      $span.wrap('<strike>');
+      $remove.removeClass('glyphicon-remove');
+      $remove.addClass('glyphicon-ok');
+      $(this).parent().insertAfter('p:last-child');
+      //console.log('Checked.');
+  } else {
+      $span.unwrap();
+      $ok.addClass('glyphicon-remove');
+      $ok.removeClass('glyphicon-ok');
+      //console.log('Not checked');
+    }
+});
+
+// Mark as priority (starred)
+
+$(list).on('click', '.glyphicon-star', function() {
+  $(this).toggleClass('active');
+  $(this).parent().insertBefore('p:first-child');
+});
+
+// Form submission prevents default behavior and appends new list item to the top
+
+$('form').on('submit', function(event) {
+  event.preventDefault();
+  var newItem = $('#todo'),
+      listItem = '<p>' +
+      '<input type="checkbox"><i class="glyphicon glyphicon-star"></i>' +
+        '<span>' + newItem.val() + '</span>' +
+        '<i class="glyphicon glyphicon-remove"></i>' +
+      '</p>';
+  if (newItem.val() !== '') {
+    $(list).prepend(listItem);
+    console.log(newItem.val());
+    newItem.val('');
+  }
+});
--- server.js
+++ server.js
@@ -5,14 +5,19 @@ var logger      = require('morgan');
 var bodyParser  = require('body-parser');
 var port        = process.env.PORT || 3000;
 var database    = 'mongodb://localhost/rgk_todo';
+// var itemRoutes  = require('./routes/item_routes.js')
 
 mongoose.connect(database, function(){
     console.log('successfully connected to: ' + database);
 });
 
+app.use(express.static('public'));
+
 app.get('/api', function(req, res){
    res.json({message: 'The router is working'});
 })
+
+// app.use('/api', itemRoutes);
 
 app.listen(port, function(){
   console.log('application server running on port: ' + port);
