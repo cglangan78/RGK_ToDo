36cfa636f49437c178a237b0ddb0dd8bf3132f1a
Kevin <kevinjustinwilliams@gmail.com>
2015-11-17 11:03:45 -0800

    add port

---
+++ server.js
@@ -0,0 +1,18 @@
+var express     = require('express');
+var app         = express();
+var mongoose    = require('mongoose');
+var logger      = require('morgan');
+var bodyParser  = require('body-parser');
+var port        = process.env.PORT || 3000;
+var database    = 'mongodb://localhost/rgk_todo';
+
+mongoose.connect(database, function(){
+    console.log('successfully connected to: ' + database);
+});
+
+
+
+
+app.listen(port, function(){
+  console.log('application server running on port: ' + port);
+})
