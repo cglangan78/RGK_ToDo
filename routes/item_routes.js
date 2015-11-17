33ab9c107fb0d1d5f4df92dd1082b5d755cbc262
Kevin <kevinjustinwilliams@gmail.com>
2015-11-17 12:16:53 -0800

    can create an item

--- routes/item_routes.js
+++ routes/item_routes.js
@@ -2,10 +2,10 @@ var itemsController   = require('../controllers/todos_controller.js');
 var express           = require('express');
 var itemRoutes        = express.Router();
 
-itemRoutes.route('/api')
+itemRoutes.route('/')
   .get(itemsController.index)
   .post(itemsController.create)
-  .delete(itemsController.destroy)
-  .put(itemsController.update);
+  //.delete(itemsController.destroy)
+  //.put(itemsController.update)
 
 module.exports = itemRoutes
