1e5aa99216d6112c3f1c5104c4a3a9326d5b8510
Kevin <kevinjustinwilliams@gmail.com>
2015-11-17 14:17:16 -0800

    changes

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -18,3 +18,27 @@ function create(req, res){
     res.json({success: true, message: 'Item was created'});
   })
 }
+
+//method to destroy an item
+function destroy(req, res){
+  Item.findOneAndRemove({item_name: req.params.item_name}, function(err){
+    if(err) console.error(err)
+    console.log(res)
+    res.json({success: true});
+  })
+}
+
+//method to update an item
+function update(req, res){
+  Item.findOneAndUpdate({item_name: req.params.item_name}, {item_name: req.body.item_name}, function(err, item){
+    if(err) console.log(err)
+    res.json({success: true, message: "Item has been updated!"});
+  })
+}
+
+module.exports = {
+  index: index,
+  create: create,
+  destroy: destroy,
+  update: update
+}
