61b69e2fac47f97b3c8612f3c6d4254012789ab2
cglangan78 <cglangan78@gmail.com>
2015-11-17 18:16:43 -0800

    trying to fix controller

--- controllers/todos_controller.js
+++ controllers/todos_controller.js
@@ -1,4 +1,3 @@
-var bodyParser  = require('body-parser');
 var Item  = require('../models/todo.js');
 
 //create index action
@@ -12,35 +11,10 @@ function index(req, res){
 //method to create an item
 function create(req, res){
   console.log('Creating an item');
-  console.log(req.body);
-  var item = new Item();
+  var item = new Item(req.body.item);
   item.item_name = req.body.item_name;
   item.save(function(err){
     if(err) console.error(err);
     res.json({success: true, message: 'Item was created'});
   })
-}
-
-//method to destroy an item
-function destroy(req, res){
-  Item.findOneAndRemove({item_name: req.params.item_name}, function(err){
-    if(err) console.error(err)
-    console.log(res)
-    res.json({success: true});
-  })
-}
-
-//method to update an item
-function update(req, res){
-  Item.findOneAndUpdate({item_name: req.params.item_name}, {item_name: req.body.item_name}, function(err, item){
-    if(err) console.log(err)
-    res.json({success: true, message: "Item has been updated!"});
-  })
-}
-
-module.exports = {
-  index: index,
-  create: create,
-  destroy: destroy,
-  update: update
 }
